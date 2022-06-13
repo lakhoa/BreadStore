e7d227c6b1a42999988830f56e5d2028a76b24dd
lakhoa <88267101+lakhoa@users.noreply.github.com>
2022-06-13 16:45:46 +0700

    gradle testing (#3)

---
+++ src/main/java/com/example/BreadStore/BreadStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.BreadStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BreadStore/BreadStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BreadStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
