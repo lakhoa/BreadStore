210546233a7c5a78f2cb86524cef2a1de5f477de
kaee <anhkhoacla@gmail.com>
2022-06-16 14:05:28 +0700

    fix pull

--- src/main/java/com/example/Miniproject_spring/repository/OrderDetailsRepository.java
+++ src/main/java/com/example/Miniproject_spring/repository/OrderDetailsRepository.java
@@ -2,6 +2,7 @@ package com.example.Miniproject_spring.repository;
 
 import com.example.Miniproject_spring.entity.Orders;
 import com.example.Miniproject_spring.entity.OrdersDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.Description;
@@ -9,7 +10,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(collectionResourceRel = "orders_detail")
-public interface OrderDetailsRepository extends PagingAndSortingRepository<OrdersDetail,Long> {
+public interface OrderDetailsRepository extends JpaRepository<OrdersDetail,Long> {
 
     @RestResource(rel = "find-by-id", description = @Description("Find by id"))
     public Orders findFirstById(@Param("id") Long id);
